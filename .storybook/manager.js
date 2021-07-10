d21cc420c99d239efb0f6ef9a94c2a31350137b7
Sergey Lukin <sergey.lukin@granulate.io>
2021-07-10 15:50:31 +0300

    upgrade

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,5 +1,4 @@
 import 'storybook-addon-jsx/register';
-import '@storybook/addon-knobs/register';
 import { addons } from '@storybook/addons';
 import theme from './theme';
 
--- components/Button.js
+++ components/Button.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import styled from '@emotion/styled';
 import isPropValid from '@emotion/is-prop-valid';
 import {
